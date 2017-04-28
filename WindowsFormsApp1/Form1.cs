92c74575e734064bc1d091ac4815556dea772f04
Immo Landwerth <immol@microsoft.com>
2017-04-27 22:10:09 -0700

    Add WinForms app

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using ClassLibrary_NetStandard20;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.Text = Class1.GetData();
+        }
+    }
+}
